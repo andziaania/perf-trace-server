a639339494871807f3214dd1c415ec538cc6d0af
andziaania <andziaania@gmail.com>
2019-09-06 13:32:13 +0200

    Initial commit - spring boot app

---
+++ src/main/java/com/pawelczyk/perftraceserver/PerftraceserverApplication.java
@@ -0,0 +1,13 @@
+package com.pawelczyk.perftraceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerftraceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerftraceserverApplication.class, args);
+	}
+
+}
