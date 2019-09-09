11a0966c4e42582ed84d2fe6648c9588751f3d6a
andziaania <andziaania@gmail.com>
2019-09-09 14:15:01 +0200

    Cleanup

--- src/main/java/com/pawelczyk/perftraceserver/converter/UsersNumberHourlyConverter.java
+++ src/main/java/com/pawelczyk/perftraceserver/converter/UsersNumberHourlyConverter.java
@@ -26,28 +26,11 @@ public class UsersNumberHourlyConverter implements AttributeConverter< List<Long
             .map(String::valueOf)
             .collect(Collectors.toList());
     return String.join(DELIMITER, usersNumberHourly);
-
-
-
-
-//    // input primitive integer array
-//    int[] intArray = { 1, 2, 3, 4, 5 };
-//
-//    String strArray[] = Arrays.stream(hours.toArray())
-//            .mapToObj(String::valueOf)
-//            .toArray(String[]::new);
-//
-//    System.out.println(Arrays.toString(strArray));
-//
-//    Arrays.stream(hours.toArray()).map
-//
-//    String usersNumberHourly = Arrays.stream(hours.toArray()).map(val -> val.toString()).reduce((a, b) -> a + DELIMITER + b, String::concat);
-//    return usersNumberHourly;
   }
 
   @Override
   public  List<Long> convertToEntityAttribute(String usersNumberHourly) {
     String[] hours = usersNumberHourly.split(DELIMITER);
-    return Arrays.stream(hours).map(val -> Long.parseLong(val)).collect(Collectors.toList());
+    return Arrays.stream(hours).map(Long::parseLong).collect(Collectors.toList());
   }
 }
